14bb4e347ef860ab70d2c637cdd0df91503d8100
Sumit <sumitkm101102@gmail.com>
2025-11-09 01:38:03 +0530

    error resolved

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector} from "react-redux";
 import { addTrailerVideo } from "../utils/moviesSlice";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { API_OPTIONS } from "../utils/constants";
 
 
@@ -9,18 +9,18 @@ const useMovieTraler = (movieId) => {
 
     const trailerVideo = useSelector(store => store.movies.trailerVideo);
     
-    const getMovieVideos = async () => {
+    const getMovieVideos = useCallback(async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/'+movieId+'/videos?language=en-US', API_OPTIONS);
         const json = await data.json();
 
         const trailerData = json.results.filter((video) => video.type === "Trailer");
         const trailer = trailerData.length ? trailerData[0] : json.results[0];
         dispatch(addTrailerVideo(trailer));
-    }
+    }, [dispatch, movieId]);
 
     useEffect(() => {
-        !trailerVideo && getMovieVideos();
-    }, []);
-}
+        if (!trailerVideo) getMovieVideos();
+    }, [trailerVideo, getMovieVideos]);
+};
 
 export default useMovieTraler;
